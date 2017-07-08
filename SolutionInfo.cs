65c240e514ca3a40619cadc2f04c7ad3a98d417d
Thaddeus Ryker <thad@edgerunner.org>
2017-07-08 17:29:13 -0400

    Built release version.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.17184.1927")]
-[assembly: AssemblyFileVersion("1.0.17184.1927")]
-[assembly: AssemblyInformationalVersion("1.0.17184.1927")]
+[assembly: AssemblyVersion("1.0.17184.1933")]
+[assembly: AssemblyFileVersion("1.0.17184.1933")]
+[assembly: AssemblyInformationalVersion("1.0.17184.1933")]
 
