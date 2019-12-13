0d8189f4b8db096f864f07e1bc58e11d6a555694
Thad Ryker <thaddeus.ryker@dansko.com>
2019-12-13 09:58:10 -0500

    Added a static guard class do to comparison benchmarks. Modified existing benchmark to add another test and more data.

---
+++ Org.Edgerunner.FluentGuard.Benchmark/Guard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Edgerunner.FluentGuard.Benchmark
+{
+   public static class Guard
+   {
+      public static void isOfType<T>(string name, object instance)
+      {
+         if (instance.GetType() != typeof(T))
+            throw new ArgumentException(string.Format("must be of type {0}", typeof(T)), name);
+      }
+
+      public static void IsNotNull(string name, object instance)
+      {
+         if (instance == null)
+            throw new ArgumentNullException(name, string.Format("{0} must not be null", name));
+      }
+
+      public static void IsWithinExclusiveBound(string name, int value, int floor, int ceiling)
+      {
+         if (value <= floor || value >= ceiling)
+            throw new ArgumentException(string.Format("{0} must be greater than {1} and less than {2}", name, floor, ceiling), name);
+      }
+   }
+}
--- Org.Edgerunner.FluentGuard.Benchmark/Program.cs
+++ Org.Edgerunner.FluentGuard.Benchmark/Program.cs
@@ -19,6 +19,7 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -33,8 +34,10 @@ namespace Org.Edgerunner.FluentGuard.Benchmark
       {
          var foo = new Dictionary<string, int> { { "test", 1 } };
          var watch = new Stopwatch();
-         double totalRun = 0;
-         int numberOfRuns = 1000000;
+         double withinBoundsCheckFluent = 0;
+         double withinBoundsCheckStatic = 0;
+         double notNullCheckFluent = 0;
+         double notNullCheckStatic = 0;
 
          // warm up
          watch.Start();
@@ -42,21 +45,54 @@ namespace Org.Edgerunner.FluentGuard.Benchmark
          validator.IsGreaterThan(0).And.IsLessThan(10).OtherwiseThrowException();
          watch.Stop();
          var firstRun = watch.Elapsed.TotalMilliseconds * 1000000.0;
+         var validator2 = Validate.That(nameof(foo), foo);
+         validator2.IsNotNull().OtherwiseThrowException();
+         Guard.IsNotNull(nameof(foo), foo);
+         Guard.IsWithinExclusiveBound(nameof(foo.Keys.Count), foo.Keys.Count, 0, 10);
 
-         // benchmark
-         for (int i = 0; i < numberOfRuns; i++)
+         // not null benchmark                 
+         for (int i = 0; i < 1000000; i++)
+         {            
+            watch.Restart();
+            validator2 = Validate.That(nameof(foo), foo);
+            validator2.IsNotNull().OtherwiseThrowException();            
+            watch.Stop();
+            notNullCheckFluent += watch.Elapsed.TotalMilliseconds;
+         }                                    
+         for (int i = 0; i < 1000000; i++)
          {
-            watch.Start();
+            watch.Restart();
+            Guard.IsNotNull(nameof(foo), foo);
+            watch.Stop();
+            notNullCheckStatic += watch.Elapsed.TotalMilliseconds;
+         }                  
+         Console.WriteLine($"First benchmark guard check: Validation that a dictionary instance is not null.");
+         Console.WriteLine($"Average execution time for guard using fluent: {notNullCheckFluent} nanoseconds");
+         Console.WriteLine($"Average execution time for guard using static: {notNullCheckStatic} nanoseconds");
+         Console.WriteLine();
+
+         // compound bound check benchmark
+         for (int i = 0; i < 1000000; i++)
+         {
+            watch.Restart();
             validator = Validate.That("foo.Keys.Count", foo.Keys.Count);
             validator.IsGreaterThan(0).And.IsLessThan(10).OtherwiseThrowException();
             watch.Stop();
-            totalRun = watch.Elapsed.TotalMilliseconds * 1000000.0;
+            withinBoundsCheckFluent += watch.Elapsed.TotalMilliseconds;
          }
-         
-         Console.WriteLine($"Guard statement: Validate.That(nameof(foo.Keys.Count), foo.Keys.Count).IsGreaterThan(0).And.IsLessThan(10).OtherwiseThrowException();");
-         Console.WriteLine($"Value: {foo.Keys.Count}");
-         Console.WriteLine($"Cold execution time: {firstRun} nanoseconds");
-         Console.WriteLine($"Average warm execution time over {numberOfRuns} runs: {totalRun / numberOfRuns} nanoseconds");
+         for (int i = 0; i < 1000000; i++)
+         {
+            watch.Restart();
+            Guard.IsWithinExclusiveBound(nameof(foo.Keys.Count), foo.Keys.Count, 0, 10);
+            watch.Stop();
+            withinBoundsCheckStatic += watch.Elapsed.TotalMilliseconds;
+         }
+
+         Console.WriteLine($"Second benchmark guard check: Validation that dictionary instance key count is greater than 0 and less than 10.");
+         Console.WriteLine($"Key count: {foo.Keys.Count}");
+         Console.WriteLine($"Cold fluent execution time: {firstRun} nanoseconds");
+         Console.WriteLine($"Average fluent warm execution time: {withinBoundsCheckFluent} nanoseconds");
+         Console.WriteLine($"Average static warm execution time: {withinBoundsCheckStatic} nanoseconds");
          Console.WriteLine("Press any key to exit benchmark");
          Console.ReadKey();
       }
